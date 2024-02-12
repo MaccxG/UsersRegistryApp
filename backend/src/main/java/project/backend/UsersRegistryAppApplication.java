5cb9e00bb0c155644de8cf0291ea2756955826ec
MaccxG <37861863+MaccxG@users.noreply.github.com>
2024-02-12 22:02:08 +0100

    created Spring Boot project

---
+++ backend/src/main/java/project/backend/UsersRegistryAppApplication.java
@@ -0,0 +1,13 @@
+package project.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersRegistryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersRegistryAppApplication.class, args);
+	}
+
+}
