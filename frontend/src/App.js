7ff953a3d4daef77563f99cc9c288c9ef400dd37
MaccxG <37861863+MaccxG@users.noreply.github.com>
2024-02-13 14:46:27 +0100

    feat: added Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Navbar from './components/navbar/Navbar'
 import Home from './components/home/Home'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
-
+      <Footer />
     </Router>
   );
 }
---
+++ frontend/src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark text-white fixed-bottom">
+      <div className="container text-center">
+        <div>
+          <a href="/contacts" className="text-white mx-2">Contacts</a>
+          <a href="/about" className="text-white mx-2">About</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer
